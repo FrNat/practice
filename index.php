d9119c2cde2c7cd096f801b39f8b16fc5ef6d655
FrNat <nat_frolov@mail.ru>
2022-06-13 16:05:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php 
+ $p = 'Приветствую Вас на моей страничке!';
+?>
+
+<?php 
+ $name = 'Александра';
+ $surname = 'Иванова';
+ $city = 'Москва';
+ $age = 28;
+ $speciality = 'Full-stack разработчик';
+?>
+
+
+<?php
+include 'main.php';
+?>
